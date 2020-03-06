03ea1279d0d7a7bc8ed5a075167d391d3e6f7ab8
zhangyu <zhangyu@example.com>
2020-03-06 23:09:42 +0800

    配置axios

--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
+import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
 import '@/style/index.less'
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+// 配置axios的baseURL   公共的请求头地址
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0'
+// 将原型属性赋值给vue对象的原型属性
+Vue.prototype.$axios = axios
 new Vue({
   router,
   render: h => h(App)

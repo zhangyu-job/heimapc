e9f96786943909e46c0a88913737c5daed972ff1
zhangyu <zhangyu@example.com>
2020-03-11 16:12:43 +0800

    素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -33,6 +33,17 @@
             </div>
         </el-tab-pane>
     </el-tabs>
+    <!-- 放置一个公共的分页组件 -->
+    <el-row type="flex" justify="center" style="height:80px" align="middle">
+        <!-- 放置分页组件
+        监听分页组件的切换事件
+         -->
+
+         <el-pagination background :total="page.total"
+         :current-page="page.currentPage"
+         :page-size="page.pageSize"
+         layout="prev,pager,next" @current-change="changePage"></el-pagination>
+    </el-row>
   </el-card>
 </template>
 
@@ -41,24 +52,40 @@ export default {
   data () {
     return {
       activeName: 'all', // 当前激活的是收藏页
-      list: [] // 全部素材的数据   接收全部素材
+      list: [], // 全部素材的数据   接收全部素材
+      // 专门的对象存放分页信息
+      page: {
+        currentPage: 1, // 默认第一页
+        total: 0, // 当前总数
+        pageSize: 8 // 每页多少条
+      }
     }
   },
   methods: {
+    // 点击分页跳转
+    changePage (newPage) {
+      this.page.currentPage = newPage // 将新页码赋值给页码数据
+      this.getMaterial() // 获取数据
+    },
     // 获取素材数据
     getMaterial () {
       this.$axios({
         url: '/user/images',
         params: {
-          collect: this.activeName === 'collect' // 根据当前页签来决定收藏素材是否显示
+          collect: this.activeName === 'collect', // 根据当前页签来决定收藏素材是否显示
+          page: this.page.currentPage, // 取页码变量中的值  只要页码变量一变获取的数也跟着变
+          per_page: this.page.pageSize
         },
         data: {} // 存放body参数
       }).then(result => {
         // 将返回的数据  赋值到data中的数据
         this.list = result.data.results
+        // 将总条数赋值给total变量
+        this.page.total = result.data.total_count
       })
     },
     changeTab () {
+      this.page.currentPage = 1 // 将页码重置为第一页   因为分类变了  数据变了
       // 点击切换事件，根据activeName来决定是否获取那个方面的数据
       this.getMaterial() // 获取素材数据
     }

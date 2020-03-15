481eeedbbf13c7d10bfc6fd8286399c418ce84d2
zhangyu <zhangyu@example.com>
2020-03-15 22:43:41 +0800

    进度条

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,10 +1,14 @@
 // 专门处理路由权限
 import router from '@/router'
-
+// 引入进度条
+import progress from 'nprogress'
+import 'nprogress/nprogress.css'
 // 注册全局前置守卫
 // 回调函数在路由发生之前执行
 // next有三种场景，放过 next()、拦截 next(false)、跳转 next(新地址，地址可以多个)
 router.beforeEach(function (to, from, next) {
+  // 开启进度条
+  progress.start()
   // 首先判断是不是去主页
   if (to.path.startsWith('/home')) {
     const token = window.localStorage.getItem('user-token')
@@ -16,4 +20,11 @@ router.beforeEach(function (to, from, next) {
   } else {
     next()
   }
+})
+
+// 在全局后置守卫的位置关闭   此时已经完成了路由的跳转
+router.afterEach(() => {
+  // 关闭进度条
+  // setTimeout(() => progress.done(), 1000)
+  progress.done()
 })

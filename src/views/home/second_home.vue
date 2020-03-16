2fcfc4563f34d91d5e0dc7ee965bc08565ea4b06
zhangyu <zhangyu@example.com>
2020-03-16 18:06:11 +0800

    使用echarts

--- src/views/home/second_home.vue
+++ src/views/home/second_home.vue
@@ -5,11 +5,103 @@
         <img :src="item" alt="">
       </el-carousel-item>
     </el-carousel>
+
+    <!-- 放置图表组件 -->
+    <el-row type="flex" justify="space-around">
+    <div ref="myChart" class="echarts"></div>
+    <div ref="myChart2" class="echarts"></div>
+
+    </el-row>
   </div>
 </template>
 
 <script>
+// 引入图标模块
+import ECharts from 'echarts'
 export default {
+  mounted () {
+    // 实例化
+    this.myChart = ECharts.init(this.$refs.myChart)
+    this.myChart2 = ECharts.init(this.$refs.myChart2)
+    this.myChart.setOption({
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [{
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line'
+      }]
+    })
+
+    this.myChart2.setOption({
+      backgroundColor: '#2c343c',
+
+      title: {
+        text: 'Customized Pie',
+        left: 'center',
+        top: 20,
+        textStyle: {
+          color: '#ccc'
+        }
+      },
+
+      tooltip: {
+        trigger: 'item',
+        formatter: '{a} <br/>{b} : {c} ({d}%)'
+      },
+
+      visualMap: {
+        show: false,
+        min: 80,
+        max: 600,
+        inRange: {
+          colorLightness: [0, 1]
+        }
+      },
+      series: [
+        {
+          name: '访问来源',
+          type: 'pie',
+          radius: '55%',
+          center: ['50%', '50%'],
+          data: [
+            { value: 335, name: '直接访问' },
+            { value: 310, name: '邮件营销' },
+            { value: 274, name: '联盟广告' },
+            { value: 235, name: '视频广告' },
+            { value: 400, name: '搜索引擎' }
+          ].sort(function (a, b) { return a.value - b.value }),
+          roseType: 'radius',
+          label: {
+            color: 'rgba(255, 255, 255, 0.3)'
+          },
+          labelLine: {
+            lineStyle: {
+              color: 'rgba(255, 255, 255, 0.3)'
+            },
+            smooth: 0.2,
+            length: 10,
+            length2: 20
+          },
+          itemStyle: {
+            color: '#c23531',
+            shadowBlur: 200,
+            shadowColor: 'rgba(0, 0, 0, 0.5)'
+          },
+
+          animationType: 'scale',
+          animationEasing: 'elasticOut',
+          animationDelay: function (idx) {
+            return Math.random() * 200
+          }
+        }
+      ]
+    })
+  },
   data () {
     return {
       list: [
@@ -31,6 +123,10 @@ export default {
   img{
     width: 100%;
     height: 100%;
+  }
+  .echarts{
+    width: 600px;
+    height: 400px;
   }
 }
 </style>
